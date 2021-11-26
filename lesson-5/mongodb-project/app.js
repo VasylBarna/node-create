b7fa3def051a9b9a51004333032f312f22446b22
VasylBarna <vasylbarna@gmail.com>
2021-11-26 12:26:04 +0200

    add lesson5

--- lesson-5/mongodb-project/app.js
+++ lesson-5/mongodb-project/app.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 const express = require('express')
 const cors = require('cors')
-// require('dotenv').config()
-const DB_HOST =
-  'mongodb+srv://BVV:5xpevFGqkKesd5WE@cluster0.rutje.mongodb.net/online_shop?retryWrites=true&w=majority'
+require('dotenv').config()
+
 const productsRouter = require('./routes/api/products')
 
 const app = express()
@@ -13,12 +12,12 @@ app.use(express.json())
 
 app.use('/api/products', productsRouter)
 
-// const { DB_HOST, PORT = 3000 } = process.env
+const { DB_HOST, PORT = 3000 } = process.env
 
 mongoose
   .connect(DB_HOST)
   .then(() => console.log('Database connect'))
-  //   .then(() => app.listen(PORT))
+  .then(() => app.listen(PORT))
   .catch((error) => {
     console.log(error.message)
     process.exit(1)
